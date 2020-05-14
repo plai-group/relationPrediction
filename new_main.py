60d2a033665b45ec980635147fbba735a95fffaf
William Harvey <wsgh@plai12.cs.ubc.ca>
2020-05-13 17:44:03 -0700

    fixes for saving / loading

--- new_main.py
+++ new_main.py
@@ -9,7 +9,9 @@ gat = GAT(
     nn_args=dict(args=GAT.adapt_args(True, args)),
     optim_args=dict(),
 )
-gat.train_n_epochs(gat.args.epochs)
+gat.set_save_valid_conditions('save', 'every', 2, 'epochs')
+gat.train_n_epochs(args.epochs_gat)
+gat.load_checkpoint(max_epochs=args.epochs_gat)  # line should be unnecessary once using latest ptutils
 
 conv = ConvDecoder(
     seed=1,
@@ -19,9 +21,10 @@ conv = ConvDecoder(
         relation_embeddings=gat.final_relation_embeddings,
     ),
     optim_args=dict(),
+    extra_things_to_use_in_hash=gat.get_path(gat.epochs),
 )
 conv.set_save_valid_conditions('save', 'every', 10, 'epochs')
-conv.train_n_epochs(conv.args.epochs)
+conv.train_n_epochs(args.epochs_conv)
 
 # fuck it
 conv.eval()
